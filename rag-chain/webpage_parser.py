760a03c0e9e989a4e8deda1c2c13774e0c8951ee
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2026-06-26 22:34:43 +0200

    Update webpage_parser.py

--- rag-chain/webpage_parser.py
+++ rag-chain/webpage_parser.py
@@ -14,9 +14,7 @@ if __name__ == "__main__":
     bs4_strainer = bs4.SoupStrainer(
         class_=("post-title", "post-header", "post-content")
     )
-    docs = load_web_page(
-        "https://lilianweng.github.io/posts/2023-06-23-agent/",
-        bs_kwargs={"parse_only": bs4_strainer},
-    )
+    
+    
     assert len(docs) == 1
     print(f"Total characters: {len(docs[0].page_content)}")
